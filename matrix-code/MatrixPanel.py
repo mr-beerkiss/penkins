99310ecba7e9dd9436fa9c619803865fed57273b
Darren Beukes <darren.beukes@gmail.com>
2016-03-17 20:43:17 +0000

    Added generic class for animation states.

---
+++ matrix-code/MatrixPanel.py
@@ -0,0 +1,72 @@
+import abc
+from multiprocessing import Process
+
+class MatrixPanel:
+	__metaclass__ = abc.ABCMeta
+
+	def __init__(self, emulation):
+		self.emulation = emulation
+
+		if (self.emulation):
+			from display import display as Adafruit_RGBMatrix
+		else:
+			from rgbmatrix import Adafruit_RGBMatrix
+
+		self.matrix = Adafruit_RGBMatrix(32, 1)
+
+	@abc.abstractmethod
+	def startLoop(self):
+		"""Provide some info"""
+		return
+
+	def exitLoop(self):
+		self.matrix.Clear()
+		return
+
+	def start(self):
+		if (self.emulation):
+			self.process = Process(target=self.matrix.start, args=(self.startLoop, self.exitLoop))
+			#self.matrix.start(self.startLoop, self.exitLoop)
+		else:
+			self.process = Process(target=self.startLoop)
+
+		self.process.start()
+		#self.process.join()
+
+		print "Returning from start"
+
+		return
+
+	def end(self):
+		self.process.terminate()
+		self.exitLoop()
+
+
+import time
+
+class TestImpl(MatrixPanel):
+	def startLoop(self):
+		self.matrix.Fill(0x0000FF)
+		time.sleep(2.0)
+		self.matrix.Clear()
+
+		for y in range(32):
+			for x in range(32):
+				self.matrix.SetPixel(x, y, 0, 0, 255)
+				time.sleep(0.025)
+
+		self.matrix.Clear()
+		return
+
+
+myTest = TestImpl(True)
+myTest.start()
+
+time.sleep(8)
+print "Sleeping time is over!"
+myTest.end()
+
+
+
+
+
