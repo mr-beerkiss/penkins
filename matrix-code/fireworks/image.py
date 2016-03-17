a7e1020e235f6c23c3bc707926800d52ffb59e54
Grzegorz Leszek <grzegorz.leszek@moonpig.com>
2016-03-17 20:57:33 +0000

    fireworks project

---
+++ matrix-code/fireworks/image.py
@@ -0,0 +1,33 @@
+emulation = False
+
+if (emulation):
+	from display import display as Adafruit_RBGmatrix
+else:
+	from rgbmatrix import Adafruit_RGBmatrix
+
+import time
+import Image
+
+matrix = Adafruit_RGBmatrix(32, 1)
+
+def run():
+	matrix.Fill(0x00FF00)
+	time.sleep(2.0)
+	matrix.Clear()
+	
+	animation = "fireworks"
+
+	for x in range(0, 42):
+		image = Image.open(x + ".png")
+		image.load()
+		matrix.SetImage(image.im.id, 0, 0)
+		time.sleep(0.3)
+
+def kill():
+	matrix.Clear()
+
+if (emulation):
+	matrix.start(run, kill)
+else:
+	run()
+
