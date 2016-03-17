10af3b5ab038102ab3c25c515fd6c6be58cc4e81
Alejo Villarrubia <alejo.villarrubia@photobox.com>
2016-03-17 14:27:00 +0000

    First version of the script that retrieves information from jenkins

---
+++ jenkins.py
@@ -0,0 +1,36 @@
+import time, sys
+
+from jenkinsapi.jenkins import Jenkins
+
+def get_dummy_build_info(jenkins, job_name):
+    job = jenkins.get_job(job_name)
+    build = job.get_last_build()
+    # print(dir(build))
+    if build == None:
+        print 'Cannot retrieve build information'
+    else:
+        status = build.get_status()
+        if status == 'SUCCESS':
+            print 'Success'
+        elif status == 'ABORTED':
+            print 'Aborted'
+        elif status == 'FAILURE':
+            print 'Failure'
+        else:
+            print 'Unknown status'
+
+if __name__ == '__main__':
+    if len(sys.argv) != 7 :
+        print 'Error. Usage -> jenkins.py -u <username> -t <token> -j <job_name>'
+        sys.exit(2)
+    else:
+        username = str(sys.argv[2])
+        token = str(sys.argv[4])
+        job_name = str(sys.argv[6])
+
+    jenkins_url = 'http://ci.boxtop.photobox.com'
+    jenkins = Jenkins(jenkins_url, username = username, password = token)
+
+    while 1==1:
+        get_dummy_build_info(jenkins, job_name)
+        time.sleep(5)
