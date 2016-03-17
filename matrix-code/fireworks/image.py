32533af740cf49c2baf2084f73fa6d6a89f265f3
Grzegorz Leszek <grzegorz.leszek@moonpig.com>
2016-03-17 21:09:26 +0000

    spaces

--- matrix-code/fireworks/image.py
+++ matrix-code/fireworks/image.py
@@ -14,12 +14,12 @@ def run():
 	matrix.Clear()
 	
 	animation = "fireworks"
-    for i in range(0, 5):
-        for x in range(0, 42):
-            image = Image.open(str(x) + ".png")
-            image.load()
-            matrix.SetImage(image.im.id, 0, 0)
-            time.sleep(0.05)
+	for i in range(0, 5):
+		for x in range(0, 42):
+			image = Image.open(str(x) + ".png")
+			image.load()
+			matrix.SetImage(image.im.id, 0, 0)
+			time.sleep(0.05)
 
 def kill():
 	matrix.Clear()
